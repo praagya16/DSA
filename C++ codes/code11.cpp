dd060e351f6e443cc1228ce0e777b426a27b8f11
praagya16 <tiwaripraagya@gmail.com>
2026-05-14 21:14:53 +0530

    Added code11 file

---
+++ C++ codes/code11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n,count=1;
+    cout<<"Enter the value of n:";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+           cout<<count;
+           
+        }
+        cout<<"\n";
+        count++;
+    }
+    return 0;
+}
