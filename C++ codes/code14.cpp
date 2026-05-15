2110764b8e8b9450be3bec2b0f80fde71bad64a0
praagya16 <tiwaripraagya@gmail.com>
2026-05-15 21:27:40 +0530

    Added code14 file

---
+++ C++ codes/code14.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n,count;
+     cout<<"Enter the value of n:";
+     cin>>n;
+
+     for(int i=1; i<=n; i++){
+        count=i;
+        for(int j=1; j<=i; j++){
+            cout<<count<<" ";
+            count--;
+        }
+        cout<<"\n";
+     }
+     return 0;
+}
