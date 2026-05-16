9ac20421d7a4bef75baaa00837ee81b962f1b67b
praagya16 <tiwaripraagya@gmail.com>
2026-05-16 14:22:32 +0530

    Added code21 file

---
+++ C++ codes/code21.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=2;
+    int b=3;
+
+    cout<<"a&b:"<<(a&b)<<"\n";
+    cout<<"a|b:"<<(a|b)<<"\n";
+    cout<<"~a:"<<~a<<"\n";
+    cout<<"a^b:"<<(a^b)<<"\n";
+
+    return 0;
+}
