25d67fd84f1cac938d451a3e8fa44e25f5f7df61
praagya16 <tiwaripraagya@gmail.com>
2026-07-01 20:37:43 +0530

    Added code40 file

---
+++ C++ codes/code40.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     
+  int i=5;
+  int* ptr=&i;
+  int** ptr2=&ptr;
+
+  cout<<ptr<<endl;
+  cout<<&i<<endl;
+  cout<<ptr2<<endl;
+  cout<<*ptr2<<endl;
+  cout<<**ptr2<<endl;
+
+   return 0;
+}
